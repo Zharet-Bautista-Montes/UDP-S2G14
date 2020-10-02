219328b9eca07e469476afb7a282983900e927ad
Zharet Bautista Montes <zd.bautista@uniandes.edu.co>
2020-10-02 12:56:16 -0500

    Inicio de repositorio

---
+++ src/core/Servidor.java
@@ -0,0 +1,23 @@
+package core;
+
+import java.io.File;
+import java.security.*;
+import javax.crypto.*;
+
+public class Servidor 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static byte[] getHashFile(String algorithm, File file)
+	{
+		MessageDigest hash = null; 
+		
+		return hash.digest(); 
+	}
+
+}
